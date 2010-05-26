4100cc722d7d4f4103a10ba39600c8f67a278d1f
Tal Atlas <me@talatlas.com>
2010-05-26 12:59:48 -0400

    round trip working. need to add tests

--- lib/csv-hash.rb
+++ lib/csv-hash.rb
@@ -35,13 +35,40 @@ module CSVHash
 
       data << hash
     end
+    
+    data
+  end
+  
+  def to_file hashes, columns
+    rows = hashes.collect do |hash|
+      vals = columns.collect do |col|
+        sp = col.split('__')
+        ret = hash
+        sp.each do |key|
+          ret = ret[key]
+        end
+        ret
+      end
+      
+      vals
+    end
+    
+    
+    csv_string = FasterCSV.generate do |csv|
+      csv << columns
+      rows.each do |val|
+        csv << val
+      end
+    end
   end
 end
 
-def CSVHash arg
+def CSVHash arg, columns=nil
   if arg.is_a?(File)
     CSVHash.from_file(arg.path)
   elsif arg.is_a?(String)
     CSVHash.from_file(arg)
+  elsif arg.is_a?(Array) && columns.is_a?(Array)
+    CSVHash.to_file(arg,columns)
   end
 end
